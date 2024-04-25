90c7d3c64b126ca3b33ec83cf212aaf724e0ccde
Justin Bantuelle <justin@mudbugmedia.com>
2024-04-25 18:34:14 -0500

    Update Node URL Path

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -29,11 +29,8 @@ install_yarn() {
 install_nodejs() {
   local version=${1:-10.x}
   local dir="${2:?}"
-
-  echo "Resolving node version $version..."
-  if ! read number url < <(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=$version" "https://nodebin.herokai.com/v1/node/$platform/latest.txt"); then
-    fail_bin_install node $version;
-  fi
+  local number=$version
+  local url="https://heroku-nodebin.s3.us-east-1.amazonaws.com/node/release/$platform/node-v$number-linux-x64.tar.gz"
 
   echo "Downloading and installing node $number..."
   local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
